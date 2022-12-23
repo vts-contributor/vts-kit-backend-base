46ce2d2d7b0aed43a12a4128d91fb0fae0fcc411
Nguyễn Văn Đạt <datnv36@viettel.com.vn>
2022-12-23 09:55:18 +0700

    UPDATED: refactor ci/cd

--- cicd/scripts/dev-build-script.sh
+++ cicd/scripts/dev-build-script.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e
 
-
-IMAGE_NAME=$1
-VERSION=$2
-
 echo "Start build maven"
 mvn -U clean install -Dmaven.test.skip=true
 echo "Finish build maven"
 
 echo "Start build docker"
-docker build -t $IMAGE_NAME:$VERSION .
+docker build -f cicd/configs/Dockerfile -t "$imageName" .
 echo "Finish build docker"
 
 echo "Push image to registry server"
-docker  push $IMAGE_NAME:$VERSION
-docker rmi $IMAGE_NAME:$VERSION
+docker push "$imageName"
+docker rmi "$imageName"
 echo "Finish push image to registry server"
+
