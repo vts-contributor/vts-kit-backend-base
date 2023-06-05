c7e4307fa073cb5c9e6351b71ddff3068cc85978
Nguyễn Văn Đạt <datnv36@viettel.com.vn>
2023-06-05 09:45:05 +0700

    UPDATED refactor script ci/cd

--- cicd/jenkinsfile/environment.groovy
+++ cicd/jenkinsfile/environment.groovy
@@ -1,46 +1,49 @@
-// Slave environment
+/** Pipeline options **/
+//env.flag_sonar = ""                                         // OPTIONAL, Enable sonar scan (set to 'yes' to enable)
+//env.flag_security = ""                                      // OPTIONAL, Enable fortify scan (set to 'yes' to enable)
 
-// Application information
-env.appName = "" // Determine docker image name (<harborServer>/<harborFolder>/<appName>:<version>_<buildNumber>)
-
-// GITLAB INFO //
-env.GITLAB_TOKEN_CREDENTIALS=""
-env.GITLAB_PROJECT_API_URL="" // example http://<serverGitLab>/api/v4/projects/4794
+/** Application information **/
+env.appName = "vts-kit-onboarding-dashboard"                                     // REQUIRED
+// Determine docker image name (<harborServer>/<harborProject>/<appName>:<version>_<buildNumber>)
+// and deployment/service name
 
 
-// Kubernetes profile (All required))
-// Development environment
-env.devKubeConfigFileSecret = ""
-env.devNamespace = ""
-env.devPort = ""
-env.devTargetPort = ""
-env.devNodePort = ""
-env.devContainerPort = ""
-env.devImagePullSecrets = ""
-// Production environment
-env.prodKubeConfigFileSecret = ""
-env.prodNamespace = ""
-env.prodPort = ""
-env.prodTargetPort = ""
-env.prodNodePort = ""
-env.prodContainerPort = ""
-env.prodImagePullSecrets = ""
+/** Kubernetes profile **/
 
-// Docker harbor configuration
-env.harborUserPassSecret = ""
-env.harborServer = ""
-env.harborProject = ""
+// Development profile
+env.devKubeConfigFileSecret = ""              // REQUIRED, Secret file credential stored on Jenkin, give in fileID
+env.devNamespace = ""                                       // REQUIRED, Kubernetes namespace to deploy
+env.devPort = ""                                          // REQUIRED, Should be nginx port
+env.devTargetPort = ""                                    // REQUIRED, Container port, should be nginx port
+env.devNodePort = ""                                        // REQUIRED, Exposal port for external access
+env.devImagePullSecrets = ""                         // REQUIRED, Docker secret to pull image
 
-// TEST config
+// Production profile
+env.prodKubeConfigFileSecret = ""            // REQUIRED, Secret file credential stored on Jenkin, give in fileID
+env.prodNamespace = ""                                      // REQUIRED, Kubernetes namespace to deploy
+env.prodPort = ""                                         // REQUIRED, Should be nginx port
+env.prodTargetPort = ""                                   // REQUIRED, Container port, should be nginx port
+env.prodNodePort = ""                                       // REQUIRED, Exposal port for external access
+env.prodImagePullSecrets = ""                        // REQUIRED, Docker secret to pull image
+
+
+/** Docker harbor configuration **/
+env.pullRegistry = ""
+env.harborProject = ""                                   // REQUIRED, Harbor folder to store image
+
+
+/** Git information **/
+env.gitTokenSecret = ""                         // REQUIRED, Secret token credential stored on Jenkin, give in secretID
+env.stagingBranch = ""                                      // OPTIONAL, Staging branch won't trigger merge build pipeline
+
+
+/** Sonar config **/
 env.maximumAllowedBugs = 0
 env.maximumAllowedVunerabilities = 0
 env.maximumAllowedCodeSmell = 0
-env.flag_sonar = ""
-// Build prefix
+
+
+/** Build prefix for building report **/
 env.pushBuildPrefix = "JENKINS-PUSH"
 env.mrBuildPrefix = "JENKINS-MERGE"
 env.acceptCloseMRBuildPrefix = "JENKINS-ACCEPT-CLOSE"
-
-///
-
-
--- cicd/scripts/dev-build-script.sh
+++ cicd/scripts/dev-build-script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-forderHarbor=$1
-nameProject=$2
 
 echo "Start build maven"
 
@@ -10,9 +8,11 @@ mvn -U clean install -Dmaven.test.skip=true
 echo "Finish build maven"
 
 echo "Start build docker"
-echo "IMAGE NAME" ${forderHarbor}/${nameProject}:latest
-docker build -f cicd/configs/Dockerfile -t ${forderHarbor}/${nameProject}:latest .
+echo "IMAGE NAME" ${harborProject}/${appName}:latest
+docker build -f cicd/configs/Dockerfile -t ${harborProject}/${appName}:latest .
 echo "Finish build docker"
+
+
 
 
 
